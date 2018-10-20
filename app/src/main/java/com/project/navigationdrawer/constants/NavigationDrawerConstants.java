20b0e925a12d5c7d78513e1b8d1a58bcd6d4faec
abdullah2213 <touchappbox@gmail.com>
2018-10-20 01:39:32 -0700

    Initial commit

---
+++ app/src/main/java/com/project/navigationdrawer/constants/NavigationDrawerConstants.java
@@ -0,0 +1,12 @@
+package com.project.navigationdrawer.constants;
+
+public class NavigationDrawerConstants {
+
+    public final static String TAG_HOME = "Home";
+    public final static String TAG_GALLERY = "Login";
+    public final static String TAG_VIDEOS = "Sign Up";
+    public final static String TAG_SETTINGS = "Settings";
+
+
+
+}
